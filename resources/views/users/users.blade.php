e56f893e30e045ea6f1b404b82eb44ca841d9c2d
cob <sanohaya2016@gmail.com>
2019-04-21 20:29:28 +0000

    user name linked followings,followers tab

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -1,14 +1,11 @@
 @if(count($users) >0)
     <ul class="list-unstyled">
         @foreach($users as $user)
-            <li class="media">
+            <li class="media mt-3">
                 <img class="mr-2 rounded" src="{{Gravatar::src($user->email, 50)}}" alt="">
                 <div class="media-body">
                     <div>
-                        {{$user->name}}
-                    </div>
-                    <div>
-                        <p>{!! link_to_route("users.show", "プロフィール", ["id" => $user->id]) !!}</p>
+                        <p>{!! link_to_route("users.show", $user->name, ["id" => $user->id]) !!}</p>
                     </div>
                 </div>
             </li>
